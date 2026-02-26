f691b014ce43bd59f8b4e8b2f5c655d592aaba0d
Ashen Herath <ashenherath75@gmail.com>
2026-02-26 07:14:16 +0000

    q2

--- DistanceConverter.java
+++ DistanceConverter.java
@@ -1,6 +1,12 @@
 public class DistanceConverter{
   public static void main(String args[]){
    
+   //We use "final" keyword to create constants.
+
+   //constants
+   final milesPerYard = 1760;
+   final kilometersPerMile = 1.609;
+    
 
    int miles = 26;
    int yards = 385;
@@ -9,7 +15,7 @@ public class DistanceConverter{
    miles = 26;
    yards = 385;
 
-  kilometers = ((miles+(1/1760)*yards))*1.609;
+  kilometers = ((miles+(1/milesPerYard)*yards))*kilometersPerMile;
 
   System.out.println("The amount of kilometers = "+kilometers);
   

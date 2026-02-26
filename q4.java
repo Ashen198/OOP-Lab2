495831c655bddd6ef6b48a7a779bfab308a152f1
Ashen Herath <ashenherath75@gmail.com>
2026-02-26 08:06:26 +0000

    q4

---
+++ q4.java
@@ -0,0 +1,14 @@
+public class q4{
+  public static void main(String args[]){
+
+    int celsius = Integer.parseInt(args[0]);
+
+    double farenheight = 1.8*celsius -32;
+   
+  
+    System.out.println("Temperature in farenheight = "+ farenheight);
+
+  }
+  
+  
+}

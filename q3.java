c35e17203ad0cd1235edd2a6b3d48c1f0d4b66f3
Ashen Herath <ashenherath75@gmail.com>
2026-02-26 07:37:55 +0000

    q3

---
+++ q3.java
@@ -0,0 +1,13 @@
+public class q3{
+  public static void main(String args[]){
+
+
+    System.out.println("Argument 1 = "+ args[0]);
+    System.out.println("Argument 2 = "+ args[1]);
+  
+    System.out.println("The length of the argument array = "+ args.length);
+
+  }
+  
+  
+}

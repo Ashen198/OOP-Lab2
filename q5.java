63e057e294c85836012127afaf397c9941893948
Ashen Herath <ashenherath75@gmail.com>
2026-02-27 08:07:03 +0000

    q5

---
+++ q5.java
@@ -0,0 +1,16 @@
+public class q5{
+  public static void main(String args[]){
+
+    double height = Double.parseDouble(args[0]);
+    double width = Double.parseDouble(args[1]);
+    double length = Double.parseDouble(args[2]);
+
+    double volume = height * width * length;
+
+  
+    System.out.println("Volume of the cube = "+ volume);
+
+  }
+  
+  
+}
